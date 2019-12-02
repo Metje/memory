89d50ab62c30522512efa041366d67d0106a0fa5
Metje Ruberg <metje.ruberg@telekom.de>
2019-12-02 08:20:36 +0100

    version 1.0

---
+++ src/com/wilders/memory/Card.java
@@ -0,0 +1,32 @@
+package com.wilders.memory;
+
+public class Card {
+
+	String pic; //Bild der Karte
+	Boolean visibility;
+	
+	// Constructor
+	public Card() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//Getters / Setters
+	private String getPic() {
+		return pic;
+	}
+
+	private void setPic(String pic) {
+		this.pic = pic;
+	}
+
+	private Boolean getVisibility() {
+		return visibility;
+	}
+
+	private void setVisibility(Boolean visibility) {
+		this.visibility = visibility;
+	}
+
+	
+
+}
